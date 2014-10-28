9ff991601ef55df433ef45a7303884cdd63712ec
Rodolfo Campos <camposer@gmail.com>
2014-10-28 10:54:38 +0100

    Implementación de Cihper simétrico con DES

--- crypto/src/crypto/cipher/TestSK.java
+++ crypto/src/crypto/cipher/TestSK.java
@@ -11,7 +11,9 @@ import javax.crypto.KeyGenerator;
 
 public class TestSK {
 	private static final String ALGORITMO = "DES";
-	
+	private static final String FICHERO_ORIGINAL = "file/quijote.txt";
+	private static final String FICHERO_CIFRADO = "file/quijote-cifrado.txt";
+	private static final String FICHERO_DESCIFRADO = "file/quijote-descifrado.txt";
 	
 	public static void main(String[] args) throws Exception {
 		// 1. Obteniendo clave simétrica
@@ -19,13 +21,33 @@ public class TestSK {
 		Key key = keyGen.generateKey();
 
 		cifrar(key);
+		System.out.println("Fichero cifrado...");
 		descifrar(key);
+		System.out.println("Fichero descifrado...");
+	}
+
+
+	private static void descifrar(Key key) throws Exception {
+		// 1. Obteniendo bytes del fichero
+		byte[] mensajeCifrado = Files.readAllBytes(new File(FICHERO_CIFRADO).toPath());
+		
+		// 2. Inicializando el motor de cifrado
+		Cipher cipher = Cipher.getInstance(ALGORITMO);
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		
+		// 3. Cifrando mensaje original
+		byte[] mensaje = cipher.doFinal(mensajeCifrado);
+		
+		// 4. Escribiendo fichero cifrado
+		Files.write(new File(FICHERO_DESCIFRADO).toPath(), 
+				mensaje, 
+				StandardOpenOption.CREATE);
 	}
 
 
 	private static void cifrar(Key key) throws Exception {
 		// 1. Obteniendo bytes del fichero
-		byte[] mensaje = Files.readAllBytes(new File("file/quijote.txt").toPath());
+		byte[] mensaje = Files.readAllBytes(new File(FICHERO_ORIGINAL).toPath());
 		
 		// 2. Inicializando el motor de cifrado
 		Cipher cipher = Cipher.getInstance(ALGORITMO);
@@ -35,8 +57,8 @@ public class TestSK {
 		byte[] mensajeCifrado = cipher.doFinal(mensaje);
 		
 		// 4. Escribiendo fichero cifrado
-		Files.write(new File("file/quijote-cifrado.txt").toPath(), 
+		Files.write(new File(FICHERO_CIFRADO).toPath(), 
 				mensajeCifrado, 
-				StandardOpenOption.CREATE_NEW);
+				StandardOpenOption.CREATE);
 	}
 }
